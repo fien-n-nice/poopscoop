0a2e8905fe4e6f77629aa2454fc349d9cea06ea0
em-t <eva.taivalsaari@gmail.com>
2020-04-11 19:33:14 +0300

    Create skeleton for project.

---
+++ app/src/main/java/nice/fien/poopscoop/MainActivity.kt
@@ -0,0 +1,18 @@
+package nice.fien.poopscoop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nice.fien.poopscoop.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/nice/fien/poopscoop/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package nice.fien.poopscoop.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
